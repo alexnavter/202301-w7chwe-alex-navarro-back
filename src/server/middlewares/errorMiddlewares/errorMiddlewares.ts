1e431b37e2102d2e2b9079d5fe20213448013f2f
Alex Navarro <alexnavter@gmail.com>
2023-02-24 18:07:02 +0100

    Fix debug module names

--- src/server/middlewares/errorMiddlewares/errorMiddlewares.ts
+++ src/server/middlewares/errorMiddlewares/errorMiddlewares.ts
@@ -2,7 +2,7 @@ import { type NextFunction, type Request, type Response } from "express";
 import createDebug from "debug";
 import { CustomError } from "../../../CustomError/CustomError";
 
-export const debug = createDebug("robots:server");
+export const debug = createDebug("users-media:server");
 
 export const notFoundError = (
   req: Request,
