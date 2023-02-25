3ef07afe8c627b75edc07e36cdd0697e49948fe0
Alex Navarro <alexnavter@gmail.com>
2023-02-25 12:24:50 +0100

    Create User schema

--- src/server/index.ts
+++ src/server/index.ts
@@ -4,8 +4,8 @@ const app = express();
 
 const port = 4000;
 
-app.get("/users-media", (req, res) => {
+app.listen(port);
+
+app.get("/", (req, res) => {
   res.status(200).json({ pong: true });
 });
-
-app.listen(port);
